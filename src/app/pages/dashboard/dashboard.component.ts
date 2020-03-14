544c66ae3191cae1fdbae8f61b00aaf018f944e3
Thomas Sanceo <tsanceo@yahoo.com>
2020-03-14 22:46:32 +0100

    chore(starting-project): generate EAN

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../shared/auth/auth.service';
+import { NzMessageService } from 'ng-zorro-antd';
 
 @Component({
     selector: 'app-dashboard',
@@ -7,7 +8,49 @@ import { AuthService } from '../../shared/auth/auth.service';
     styleUrls: ['./dashboard.component.less'],
 })
 export class DashboardComponent implements OnInit {
-    constructor(public authService: AuthService) {}
+    public value: string;
+    constructor(public authService: AuthService, private message: NzMessageService) {}
 
-    ngOnInit() {}
+    ngOnInit() {
+        this.generateEAN();
+    }
+
+    generateEAN() {
+        const results = [];
+        const ponderations = [1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3];
+        const rdmByPonderation = [];
+        const divider = 10;
+        let i = 0;
+        while (i <= 11) {
+            results.push(Math.floor(Math.random() * 10));
+            i++;
+        }
+        ponderations.forEach((ponderation, index) => {
+            rdmByPonderation.push(ponderation * results[index]);
+        });
+        const totalRdmByPonderation = rdmByPonderation.reduce((a, b) => a + b, 0);
+        const restDivisionTotalRdmByPonderationAndDivider = totalRdmByPonderation % divider;
+        const keyEAN13 = divider - restDivisionTotalRdmByPonderationAndDivider;
+        if (restDivisionTotalRdmByPonderationAndDivider === 0) {
+            results.push(0);
+        } else {
+            results.push(keyEAN13);
+        }
+        this.value = results.join('');
+    }
+
+    copyMessage(val: string) {
+        const selBox = document.createElement('textarea');
+        selBox.style.position = 'fixed';
+        selBox.style.left = '0';
+        selBox.style.top = '0';
+        selBox.style.opacity = '0';
+        selBox.value = val;
+        document.body.appendChild(selBox);
+        selBox.focus();
+        selBox.select();
+        document.execCommand('copy');
+        document.body.removeChild(selBox);
+        this.message.create('success', `Le code EAN est prêt à être coller.`);
+    }
 }
